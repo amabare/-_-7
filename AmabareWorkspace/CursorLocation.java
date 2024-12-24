3393e14cc53564fd91031aac166ef3483eac70eb
amabare <260375b@ugs.kochi-tech.ac.jp>
2024-12-24 15:58:43 +0900

    モジュールとしての機能できる必要最小限のものにした。ただ、返り値としてint型の配列(カーソルのx座標, カーソルのy座標)が設定されているので注意

--- AmabareWorkspace/CursorLocation.java
+++ AmabareWorkspace/CursorLocation.java
@@ -2,11 +2,9 @@
 ソースコード作成者: 山本
 修正日時: 2024/12/24
 ソースコードの動作や役割: マウスカーソルの位置情報を取得する
-コメント: 返り値として「return 0;」を設定することができていない
-　　　　  あと、
-　　　　  　・":"の後にスペースを入れることができていない
-　　　　  　・"()"の後に"{}"を書く際はスペースを入れられてない
-　　　　  入れることができないのかもしれない
+コメント: 返り値として int 型の配列(カーソルのx座標, カーソルのy座標)を返す
+　　　　  呼び出すたびにカーソルの位置情報を取得する関係上、呼び出し元では無限ループを使用する必要がある
+　　　　  無限ループを脱するためには「ボタンを押す行為があればbreak」などとすれば良い
 */
 
 package AmabareWorkspace;
@@ -15,38 +13,13 @@ package AmabareWorkspace;
 import java.awt.MouseInfo;
 // 2次元座標を表すためのクラス
 import java.awt.Point;
-// トップレベルのウィンドウを表すためのクラス
-import javax.swing.JFrame;
-// テキストやアイコンを表示するためのクラス
-import javax.swing.JLabel;
-// 指定された間隔でイベントを発生させるためのクラス
-import javax.swing.Timer;
 
-public class CursorLocation extends JFrame {
-    private JLabel label;
-
-    // コンストラクタ
-    public CursorLocation() {
-        label = new JLabel("カーソルの位置: ");
-        // labelの文字列をフレームに追加
-        add(label);
-        // フレームのサイズを設定
-        setSize(300, 200);
-        // ウィンドウが閉じられたときアプリケーションを終了する
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // ウィンドウを表示
-        setVisible(true);
-
-        // 100ミリ秒ごとにカーソルの位置を更新
-        Timer timer = new Timer(100, _ -> UpdateCursorLocation());
-        // タイマーを開始
-        timer.start();
-    }
-
+public class CursorLocation {
     // メソッド
-    private void UpdateCursorLocation() {
+    public int[] UpdateCursorLocation() {
         // マウスカーソルの現在位置を取得
         Point point = MouseInfo.getPointerInfo().getLocation();
-        label.setText("カーソルの位置: X=" + point.x + " Y=" + point.y);
+        // 返り値としてx座標とy座標を返す
+        return new int[] { point.x, point.y };
     }
 }
