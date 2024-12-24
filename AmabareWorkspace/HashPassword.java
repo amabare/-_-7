09b4f3ef63a49b1512e2d5752eb7f90f11aa5b8e
amabare <260375b@ugs.kochi-tech.ac.jp>
2024-12-24 16:51:46 +0900

    コーディング規約との照らし合わせを行った

--- AmabareWorkspace/HashPassword.java
+++ AmabareWorkspace/HashPassword.java
@@ -1,3 +1,10 @@
+/*
+ * ソースコード作成者: 山本
+ * 修正日時: 2024/12/24 16:39
+ * ソースコードの動作や役割: パスワードのハッシュ化を行う
+ * コメント: ハッシュ化を行うために「SHA-256」アルゴリズムを使用した
+ */
+
 package AmabareWorkspace;
 
 // SHA-256 などのアルゴリズムを用いてデータのハッシュ値を求めることができる
@@ -6,25 +13,28 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashPassword {
+
+    // ハッシュ化された文字列を返すメソッド
     public static String HashString(String input) {
         try {
             // MessageDigest インスタンスを取得 (アルゴリズムは SHA-256 を使用)
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            MessageDigest msg = MessageDigest.getInstance("SHA-256");
 
             // 入力文字列をバイト配列に変換し、ハッシュ値を計算
-            byte[] hashBytes = md.digest(input.getBytes());
+            byte[] hashbyte = msg.digest(input.getBytes());
 
             // バイト配列を16進数の文字列に変換
             // StringBuilder クラスを使用することで文字列の連結を高速化、可変の文字列を扱える
-            StringBuilder sb = new StringBuilder();
-            for (byte b : hashBytes) {
+            StringBuilder str = new StringBuilder();
+            // bstr には hashbyte の要素が順番に格納される
+            for (byte bstr : hashbyte) {
                 // 1 バイトの値を 16 進数の文字列に変換
                 // (例: 0x0A -> "0a", 0xFF -> "ff")
-                sb.append(String.format("%02x", b));
+                str.append(String.format("%02x", bstr));
             }
 
             // ハッシュ値の文字列を返す
-            return sb.toString();
+            return str.toString();
         } // NoSuchAlgorithmException がスローされた場合の例外処理
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
