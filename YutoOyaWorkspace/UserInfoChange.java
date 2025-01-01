3d11d8f2f44f309f31123d9b7e1402b1f8760bd2
1260296-Oya-Yuto <260296c@ugs.kochi-tech.ac.jp>
2025-01-02 08:53:06 +0900

    htmlファイルのファイルのみ作成 まだ中は作ってない

--- YutoOyaWorkspace/UserInfoChange.java
+++ YutoOyaWorkspace/UserInfoChange.java
@@ -1,5 +1,19 @@
+/* 
+ソースコード作成者: 大矢
+修正日時: 2025/1/1
+ソースコードの動作や役割: ユーザの登録情報を変更する
+コメント: 基本的にはDB操作を呼び出すのみ
+*/
+
 package YutoOyaWorkspace;
 
 public class UserInfoChange {
-    
+    //メソッド
+    public static int Userinfochange(){
+        //入力された情報を取得(Settext?)
+
+        //UpdateDBでデータベースに格納する
+
+        return 0;
+    }
 }
